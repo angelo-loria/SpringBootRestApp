dc04d225db9d8b52a38398841d3974d57d1eb3fc
aloria <aloria@haworthmedia.com>
2021-02-23 15:24:26 -0600

    add mock beans, update test

--- src/main/java/com/angelo/restservice/RestApp/FancyWriter.java
+++ src/main/java/com/angelo/restservice/RestApp/FancyWriter.java
@@ -1,6 +1,7 @@
 package com.angelo.restservice.RestApp;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Qualifier("FancyWriter")
--- src/test/java/com/angelo/restservice/RestApp/HelloWorldTest.java
+++ src/test/java/com/angelo/restservice/RestApp/HelloWorldTest.java
@@ -3,11 +3,15 @@ package com.angelo.restservice.RestApp;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,14 +26,22 @@ class HelloWorldTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    @Qualifier("RegularWriter")
+    TextWriter regular;
+
+    @MockBean
+    @Qualifier("FancyWriter")
+    TextWriter fancy;
 
     @Test
     void testHappy() throws Exception{
+        when(fancy.writeText("hi hello")).thenReturn("hi mock text");
         MvcResult result = this.mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
 
-        assertEquals(result.getResponse().getContentAsString(), "hi hello fancy text");
+        assertEquals(result.getResponse().getContentAsString(), "hi mock text");
     }
 }
