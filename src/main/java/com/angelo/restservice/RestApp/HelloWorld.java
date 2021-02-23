fc22d0c77cc763f43ec8f857777961fb636781b6
aloria <aloria@haworthmedia.com>
2021-02-23 14:02:07 -0600

    init project

---
+++ src/main/java/com/angelo/restservice/RestApp/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.angelo.restservice.RestApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index() {
+        return "hello world";
+    }
+
+}
---
+++ src/test/java/com/angelo/restservice/RestApp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelo.restservice.RestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
